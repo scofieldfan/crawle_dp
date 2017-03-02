f6c92e2883397cbeeca45a13098639a652eec380
fanzhang <scofieldfan@163.com>
2017-03-02 21:33:40 +0800

    添加支持多种接口

--- src/hot.js
+++ src/hot.js
@@ -5,7 +5,6 @@
 var File = require('./file');
 var Crawler = require("crawler");
 var dateFormat = require('dateformat');
-var outputFile = __dirname + '/../data/hot.csv';
 var cityObj = require('./city');
 var citys = cityObj.citys;
 var listCrawler = new Crawler({
@@ -16,38 +15,45 @@ var listCrawler = new Crawler({
         if (error) {
             console.error(error);
         } else {
+            var paths = res.request.uri.pathname.split("/");
+            var type  = paths[paths.length-1];
+            var fileName = __dirname + `/../data/${type}.csv`;
             var data = JSON.parse(res.body).data;
-            console.log("begin crawle...",res.request.uri.path);
+            console.log("begin crawle...", res.request.uri.path);
             if (data && data.rank && data.rank.list) {
                 var list = JSON.parse(data.rank.list);
-                var cityArray =  cityObj.findCity(data.rank.req.provinceid,data.rank.req.cityid);
-                var outputArray =[
+                var cityArray = cityObj.findCity(data.rank.req.provinceid, data.rank.req.cityid);
+                var outputArray = [
                     cityArray[0],
                     cityArray[1],
                     data.rank.req.date
                 ]
-                if(list){
+                if (list) {
                     outputArray = outputArray.concat(list.map((item) => item.dimname));
                 }
-                File.appendFile(outputFile, outputArray.join(",")+"\r\n");
+                File.appendFile(fileName, outputArray.join(",") + "\r\n");
             }
         }
         done();
     }
 });
-citys.forEach(function (item) {
+var types = ['dish', 'consumer', 'popular', 'happiness','hot'];
+types.forEach((type)=> {
+    citys.forEach((item) => {
+        for (var d = 0; d < 2; d++) {
+            var date = new Date();
+            date.setDate(date.getDate() - d);
+            var day = dateFormat(date, "yyyy-mm-dd");
+            var crawlerUrl = `http://m.dianping.com/datamap/newyearmapapi/food/${type}?date=${day}&provinceid=${item.pId}`;
+            listCrawler.queue(crawlerUrl);
+            item.cList.forEach((city) => {
+                var cId = city.cId;
+                var url = `http://m.dianping.com/datamap/newyearmapapi/food/${type}?date=${day}&provinceid=${item.pId}&cityid=${cId}`;
+                listCrawler.queue(url);
+            })
+        }
+    });
 
-    for(var d = 0;d<2;d++){
-        var date = new Date();
-        date.setDate(date.getDate() - d);
-        var day = dateFormat(date, "yyyy-mm-dd");
-        var hot = `http://m.dianping.com/datamap/newyearmapapi/food/hot?date=${day}&provinceid=${item.pId}`;
-        listCrawler.queue(hot);
-        item.cList.forEach(function(city){
-            var cId = city.cId;
-            var url = `http://m.dianping.com/datamap/newyearmapapi/food/hot?date=${day}&provinceid=${item.pId}&cityid=${cId}`;
-            listCrawler.queue(url);
-        })
-    }
-});
+})
+
 
